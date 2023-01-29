921a6c9b18a1de2804823794812bcfbbf9d71945
Teppei Torii <teppeichan9@gmail.com>
2023-01-29 16:33:50 +0900

    Chore: Postsテーブルに初期データを生成

--- src/database/factories/PostFactory.php
+++ src/database/factories/PostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PostFactory extends Factory
@@ -11,10 +12,18 @@ class PostFactory extends Factory
      *
      * @return array
      */
+    protected $model = Post::class;
+
     public function definition()
     {
+        $random_date = $this->faker->dateTimeBetween('-1year', '-1day');
         return [
-            //
+            'title' => $this->faker->realText(rand(20, 50)),
+            'body' => $this->faker->realText(rand(100, 200)),
+            'is_public' => $this->faker->boolean(90),
+            'published_at' => $random_date,
+            'created_at' => $random_date,
+            'updated_at' => $random_date
         ];
     }
 }
